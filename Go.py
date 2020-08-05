c9d96eb7db1beb3fd35798e9c538f065f4be4091
Pricneton Wong <princetonwong2@gmail.com>
2020-08-05 19:29:54 +0800

    Aug 5 1929 update

--- Go.py
+++ Go.py
@@ -1,9 +1,9 @@
 from CustomAPI.Execution import Execution
-from CustomAPI.FutuAPI import FutuAPI
+
 from futu import *
 import CustomAPI.Helper as helper
 
-HK_HSIConstituent_code = FutuAPI().getPlateStock("HK.HSI Constituent")["code"].values.tolist()
+# from CustomAPI.FutuAPI import FutuAPI ; HK_HSIConstituent_code = FutuAPI().getPlateStock("HK.HSI Constituent")["code"].values.tolist()
 HKStockSubtypes = [SubType.K_1M, SubType.K_3M, SubType.K_5M, SubType.K_15M, SubType.K_30M, SubType.K_60M]
 USOptionSubTypes = [SubType.K_DAY, SubType.K_1M, SubType.K_5M, SubType.K_15M, SubType.K_60M]
 
