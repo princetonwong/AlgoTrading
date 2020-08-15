14e08c49ecf08fe62cff5abc6e48a6706375899a
jonl0405 <69384866+jonl0405@users.noreply.github.com>
2020-08-15 19:32:27 +0800

    Added BollingerBandsStrategy

--- BacktraderAPI/BTStrategy.py
+++ BacktraderAPI/BTStrategy.py
@@ -1,6 +1,8 @@
 import backtrader as bt
 from BacktraderAPI import BTIndicator
 
+#All bt indicators: https://www.backtrader.com/docu/indautoref/
+
 class BuyWhenCloseLessThanTwoPreviousStrategy(bt.Strategy):
 
     def log(self, txt, dt=None):
@@ -197,7 +199,7 @@ class RSIStrategy(bt.Strategy):
             elif self.rsi < self.p.lowerband:
                 self.buy(size=1)
 
-class maCross(bt.Strategy):
+class MACrossStrategy(bt.Strategy):
     '''
     For an official backtrader blog on this topic please take a look at:
 
@@ -255,3 +257,87 @@ class maCross(bt.Strategy):
                                                 trade.data._name,
                                                 round(trade.pnl,2),
                                                 round(trade.pnlcomm,2)))
+
+class BollingerBandsStrategy(bt.Strategy):
+
+    #https://backtest-rookies.com/2018/02/23/backtrader-bollinger-mean-reversion-strategy/
+
+    '''
+    This is a simple mean reversion bollinger band strategy.
+
+     Entry Critria:
+      - Long:
+          - Price closes below the lower band
+          - Stop Order entry when price crosses back above the lower band
+      - Short:
+          - Price closes above the upper band
+          - Stop order entry when price crosses back below the upper band
+     Exit Critria
+      - Long/Short: Price touching the median line
+    '''
+
+    params = (
+        ("period", 20),
+        ("devfactor", 3),
+        ("size", 20),
+        ("debug", False)
+              )
+
+    def __init__(self):
+        self.boll = bt.indicators.BollingerBands(period=self.p.period, devfactor=self.p.devfactor)
+        # self.sx = bt.indicators.CrossDown(self.data.close, self.boll.lines.top)
+        # self.lx = bt.indicators.CrossUp(self.data.close, self.boll.lines.bot)
+
+    def next(self):
+
+        orders = self.broker.get_orders_open()
+
+        # Cancel open orders so we can track the median line
+        if orders:
+            for order in orders:
+                self.broker.cancel(order)
+
+        if not self.position:
+
+            if self.data.close > self.boll.lines.top:
+                self.sell(exectype=bt.Order.Stop, price=self.boll.lines.top[0], size=self.p.size)
+
+            if self.data.close < self.boll.lines.bot:
+                self.buy(exectype=bt.Order.Stop, price=self.boll.lines.bot[0], size=self.p.size)
+
+        else:
+
+            if self.position.size > 0:
+                self.sell(exectype=bt.Order.Limit, price=self.boll.lines.mid[0], size=self.p.size)
+                if self.data.close < self.boll.lines.bot:
+                    self.buy(exectype=bt.Order.Stop, price=self.boll.lines.bot[0], size=self.p.size)
+            else:
+                self.buy(exectype=bt.Order.Limit, price=self.boll.lines.mid[0], size=self.p.size)
+                if self.data.close > self.boll.lines.top:
+                    self.sell(exectype=bt.Order.Stop, price=self.boll.lines.top[0], size=self.p.size)
+
+        if self.p.debug:
+            print('---------------------------- NEXT ----------------------------------')
+            print("1: Data Name:                            {}".format(data._name))
+            print("2: Bar Num:                              {}".format(len(data)))
+            print("3: Current date:                         {}".format(data.datetime.datetime()))
+            print('4: Open:                                 {}'.format(data.open[0]))
+            print('5: High:                                 {}'.format(data.high[0]))
+            print('6: Low:                                  {}'.format(data.low[0]))
+            print('7: Close:                                {}'.format(data.close[0]))
+            print('8: Volume:                               {}'.format(data.volume[0]))
+            print('9: Position Size:                       {}'.format(self.position.size))
+            print('--------------------------------------------------------------------')
+
+    def notify_trade(self, trade):
+        if trade.isclosed:
+            dt = self.data.datetime.date()
+
+            print('---------------------------- TRADE ---------------------------------')
+            print("1: Data Name:                            {}".format(trade.data._name))
+            print("2: Bar Num:                              {}".format(len(trade.data)))
+            print("3: Current date:                         {}".format(dt))
+            print('4: Status:                               Trade Complete')
+            print('5: Ref:                                  {}'.format(trade.ref))
+            print('6: PnL:                                  {}'.format(round(trade.pnl, 2)))
+            print('--------------------------------------------------------------------')
