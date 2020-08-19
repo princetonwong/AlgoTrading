0c13f4bdd6c7ec50129b3c4ba09554f81192ca0a
jonl0405 <69384866+jonl0405@users.noreply.github.com>
2020-08-20 01:10:15 +0800

    Added WilliamsRStrategy

--- BacktraderAPI/BTStrategy.py
+++ BacktraderAPI/BTStrategy.py
@@ -250,6 +250,57 @@ class MACDCrossStrategy(bt.Strategy):
         #         # Update only if greater than
         #         self.pstop = max(pstop, pclose - pdist)
 
+class WilliamsRStrategy(bt.Strategy):
+
+    '''
+    Entry Criteria:
+      - Long:
+          - Price close crosses down lowerband and then crosses up back
+      - Short:
+          - Price close crosses up upperband and then crosses down back
+     Exit Criteria:
+      - Long/Short:  Price close crosses up upperband / down lowerband
+    '''
+
+    params = (
+        ('period', 14),
+        ('upperband', -40),
+        ('lowerband', -60),
+        ("debug", False)
+             )
+
+    def __init__(self):
+        self.williamsR = bt.indicators.WilliamsR(self.data,
+                                                 period=self.p.period,
+                                                 upperband=self.p.upperband,
+                                                 lowerband=self.p.lowerband,
+                                                 )
+        self.crossLowerBand = bt.indicators.CrossOver(self.williamsR, self.p.lowerband, subplot=False)
+        self.crossUpperBand = bt.indicators.CrossOver(self.williamsR, self.p.upperband, subplot=False)
+
+    def next(self):
+        orders = self.broker.get_orders_open()
+
+        if self.position.size == 0:  # not in the market
+
+            if self.crossLowerBand == -1:
+                self.buy(exectype=bt.Order.Stop, price=self.data.close)
+            if self.crossUpperBand == -1:
+                self.sell(exectype=bt.Order.Stop, price=self.data.close)
+
+        elif self.position.size > 0:  # longing in the market
+
+            if self.crossUpperBand == 1:
+                self.sell(exectype=bt.Order.Stop, price=self.data.close)
+
+        elif self.position.size < 0:  # shorting in the market
+
+            if self.crossLowerBand == 1:
+                self.buy(exectype=bt.Order.Stop, price=self.data.close)
+
+        if self.p.debug:
+            self.debug()
+
 #Trend Following
 class DonchianStrategy(bt.Strategy):
     def __init__(self):
