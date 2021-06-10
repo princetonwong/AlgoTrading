8f0cc5a648545964fa6a611cb91c43b418fb74e2
Pricneton Wong <princetonwong2@gmail.com>
2021-06-10 15:12:02 +0800

    Added Keys

---
+++ Keys.py
@@ -0,0 +1,13 @@
+IEX_TOKEN='pk_24af1472c0a048c3ac1a24a0d10b6040'
+APCA_API_BASE_URL='https://paper-api.alpaca.markets'
+APCA_API_KEY_ID='PKOEWL614X66ETVMHPX2'
+APCA_API_SECRET_KEY='y9k01pW7yA58oymI1nZheUpA79uG8GwW3QPvae73'
+FutuPassword = "147536"
+AlphaVantage_API_KEY = "WRCGUKGKU6JFU4RK"
+Telegram_api_id = 2934476
+Telegram_api_hash = 'a30c1aaeb0e990c018399e0ce50f461c'
+Telegram_algolab = 1001478581063
+Telegram_Princeton = 432454473
+Telegram_Shuttlealgo = 1001199220134
+Telegram_TinTinTrader = 1001247831376
+
